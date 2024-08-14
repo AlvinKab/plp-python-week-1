984dc83bbcfe5502b3584dd420734edafb753b5f
AlvinKab <alkmwas@gmail.com>
2024-08-14 09:58:18 +0300

    fulfilled week 1 assignment requirements

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+loc = input("Enter your location: ")
+
+print(f"Your name is {name}, you are {age} years old, and you live in {loc}.")
